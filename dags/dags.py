aa977429be0d1743d8876889f724a4f0cf81200b
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-10-23 20:29:41 -0300

    config conexão

---
+++ dags/dags.py
@@ -0,0 +1,16 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+
+
+
+with DAG(
+    dag_id="example_python_operator",
+    schedule=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["example"],
+) as dag:
+    inicio_dag = EmptyOperator(
+        task_id='inicio_dag'
+    )
